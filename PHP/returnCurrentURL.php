4df3ee58cf6e55bf4ef771bea65f4817db312467
SVENSCHULZE <contact@svenschulze.com>
2014-01-17 17:58:07 +0000

    Return the current url

---
+++ PHP/returnCurrentURL.php
@@ -0,0 +1,18 @@
+<?php
+/* Return the current url (adressbar)
+ */
+function curPageURL() {
+	/* start with http */
+ 	$pageURL = 'http';
+	/* check SSL */
+ 	if ($_SERVER["HTTPS"] == "on") { $pageURL .= "s"; }
+ 	$pageURL .= "://";
+	/* check port */
+ 	if ($_SERVER["SERVER_PORT"] != "80") {
+	  	$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+	} else {
+  		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ 	}
+ return $pageURL;
+}
+?>
